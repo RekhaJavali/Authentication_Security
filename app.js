9ffa349ff21f2771e9d20776cf68c14ec45e4981
REKHAE JAVALI <rekhajavali22@gmal.com>
2023-02-20 01:52:25 +0530

    level3 encryption

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt =  require("mongoose-encryption"); //to encrypt password in db
 
+const md5 = require("md5");
 
 app.use(bodyParser.urlencoded({extended:true}));
 // console.log(process.env.SECRET);
@@ -28,7 +29,7 @@ const userSchema =  new mongoose.Schema({
 
 
 // const secret = "MynameisRekhaGKfromDavanagere"; //in env
-userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields: ["password"] }); // removed for hasing md5level3
 
 const User = mongoose.model("User", userSchema);
 
@@ -51,7 +52,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser =  new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err) console.log(err);
@@ -64,7 +65,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username =  req.body.username;
-    const password =  req.body.password;
+    const password =  md5(req.body.password);
     console.log(username);
     console.log(password);
 

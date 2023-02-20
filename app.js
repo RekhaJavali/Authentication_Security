d099b64ea1a6a82e353af21a123db13cb5397ff4
REKHAE JAVALI <rekhajavali22@gmal.com>
2023-02-21 02:02:50 +0530

    level 4 bcrypt

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt =  require("mongoose-encryption"); //to encrypt password in db
-
 const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({extended:true}));
 // console.log(process.env.SECRET);
@@ -50,22 +51,31 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req, res){
-    const newUser =  new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser =  new User({
+            email: req.body.username,
+            // password: md5(req.body.password) //level3 security
+            password: hash //level4 sec
+        });
+        newUser.save(function(err){
+            if(err) console.log(err);
+            else res.render("secrets");
+        });
+        console.log(newUser.email);
+        console.log(newUser.password);
     });
-    newUser.save(function(err){
-        if(err) console.log(err);
-        else res.render("secrets");
-    });
-    console.log(newUser.email);
-    console.log(newUser.password);
+
+
+    
     
 });
 
 app.post("/login", function(req, res){
     const username =  req.body.username;
-    const password =  md5(req.body.password);
+    // const password =  md5(req.body.password);//level3 encryption
+    const password = req.body.password //level4
     console.log(username);
     console.log(password);
 
@@ -74,14 +84,18 @@ app.post("/login", function(req, res){
             console.log(err);
         else{
             if(founduser){
-                if(founduser.password === password){
-                    res.render("secrets");
+                // if(founduser.password === password){ //level3 commenting to implement level4
+                bcrypt.compare(password, founduser.password, function(err, result) {
+                    if(result === true)
+                        res.render("secrets");
+                });
+                    
                 }
                 
             }
-        }
+        });
     });
-});
+
 
 app.listen("3000", function(){
     console.log("server on 3000 port");
